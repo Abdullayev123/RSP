a8261e6d5acfaa7646b169e8ec77c07ec2b0b5d0
Abdullayev123 <xezeraa-bp216@code.edu.az>
2024-10-28 09:53:59 +0400

    Adding User Icon

--- script.js
+++ script.js
@@ -4,17 +4,31 @@ const scissors = document.querySelector(".scissors");
 const result = document.querySelector(".result h1");
 const playerScore = document.querySelector(".playerScore");
 const computerScore = document.querySelector(".computerScore");
+const myChoise = document.querySelector(".my_choise");
+const icon = document.createElement("i");
+icon.classList.add("fa-regular");
+
 let cScore = 0;
 let pScore = 0;
 function playerSelection() {
   rock.addEventListener("click", () => {
     playRound("rock");
+    icon.classList.add("fa-hand-back-fist");
+    myChoise.appendChild(icon);
   });
   paper.addEventListener("click", () => {
     playRound("paper");
+    icon.classList.remove("fa-hand-scissors");
+    icon.classList.remove("fa-hand-back-fist");
+    icon.classList.add("fa-hand");
+    myChoise.appendChild(icon);
   });
   scissors.addEventListener("click", () => {
     playRound("scissors");
+    icon.classList.remove("fa-hand");
+    icon.classList.remove("fa-hand-back-fist");
+    icon.classList.add("fa-hand-scissors");
+    myChoise.appendChild(icon);
   });
 }
 
@@ -23,6 +37,7 @@ function playRound(playerSelection) {
   const randomChoise = choices[Math.floor(Math.random() * choices.length)];
   if (playerSelection === "rock" && randomChoise === "paper") {
     result.innerHTML = "You Lose 😓! ";
+
     cScore += 1;
     computerScore.innerHTML = cScore;
   } else if (playerSelection === "paper" && randomChoise === "scissors") {
